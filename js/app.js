f01e00f9df4a2b10337686372aa7d85349c37e94
arianiapalmer <arianiapalmer@gmail.com>
2022-06-10 01:23:17 -0400

    added the 5 questions, still cant get them to prompt individually

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 'use strict'
-
+// user welcome message
 let firstName = prompt("Hey! What's your name?");
 
 console.log("users name is " + firstName);
 
-alert("Hey" + firstName + "!");
+alert("Hey " + firstName + "!");
+
+// gettinging to know the user
 
 let age = prompt("How old are you?");{
 
     console.log("user is " + age)
 
-    alert("WOW" + age + " you're really getting up there huh");
+    alert("WOW " + age + " you're really getting up there huh");
 
 }
 
@@ -26,19 +28,18 @@ let time = prompt("What time zone do you live in?");{
 
     console.log("the user's time zone is " + time)
 
-    if(time == "Eastern"){
+    if(time == "eastern"){
 
-         alert ("Hey! Same here!");
+        alert ("Hey! Same here!");
 
        }
 
        else{
+          console.log('user does not live in the same time zone as ari')
 
-            console.log('user does not live in the same time zone as ari')
+          alert("I don't live in " + time + " I wonder what our time difference is");
 
-           alert("I don't live in " + time + " I wonder what our time difference is");
-
-           }
+        }
 
 }
 
@@ -50,11 +51,50 @@ let sleep = prompt ("what time do you usually sleep?");{
 
 }
 
-function hello(){
-  alert("hello");
+// user gets to know me, my yes or no questions
+
+function YorN(){
+
+  let questions = [
+    'Am I over 25?',
+    'Do I have dogs?',
+    'Was I born in Ohio?',
+    'Do I love anime?',
+    'Am I a Sagittarius?',
+  ]
+  prompt(questions);
+    questions[1] = false;
+    questions[2] = false;
+    questions[3] = false;
+    questions[4] = confirm;
+    questions[5] = confirm;
+    for (let questions = 0; questions < questions.length; questions++){
+
+    if(questions = false)
+    alert ('You just won brownie points!');
+    else {
+      if (confirm)
+      alert ('Wow, so am I invisible to you?');
+    }
+  }
+
+  // yes = Boolean(false);
+  // no = Boolean(true);\
+
+  // if (ariAge) = false; 
+  // alert 
+
+  // if ariPet
+  // if ariBorn
+  // if ariLuv
+  // if ariSag
+  
 }
 
-hello();
+
+
+
+YorN();
 
 //document.write("Hi " + firstName + "you are " + age + ", you live in " + live + "operating on " + time + " but you sleep at " + sleep + " your time. Glad to have you in class :\)");
 
